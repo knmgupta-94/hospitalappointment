7f25682eac804bc9f4b94e97e5e14f5fc0ef96f8
KNM Gupta <knmgupta94@gmail.com>
2026-06-21 11:19:53 +0530

    Fix import case for TopDoctors component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Speciality from '../components/Speciality'
-import TopDoctors from '../components/TopDoctors'
+import Topdoctors from '../components/Topdoctors'
 import Banner from '../components/Banner'
 
 const Home = () => {
